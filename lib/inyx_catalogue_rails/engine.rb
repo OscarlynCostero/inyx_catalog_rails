047d9e2acf8666b1ed9d15898df9cd1b9fde585d
Gabriel Morillo <gbrlmrllo@gmail.com>
2014-11-24 16:45:45 -0430

    loadpath
    path

--- lib/inyx_catalogue_rails/engine.rb
+++ lib/inyx_catalogue_rails/engine.rb
@@ -1,5 +1,6 @@
 module InyxCatalogueRails
   class Engine < ::Rails::Engine
+    config.autoload_paths << File.expand_path("../lib/some/path", __FILE__)
     isolate_namespace InyxCatalogueRails
   end
 end
