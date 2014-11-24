f62c41324f78599d9c7cdd04b646f8d2542b12f9
gbrlmrllo <gbrlmrllo@gbrlmrllo-desktop.(none)>
2014-11-24 17:48:35 -0430

    add ImageUploader to module

--- lib/inyx_catalogue_rails/engine.rb
+++ lib/inyx_catalogue_rails/engine.rb
@@ -1,6 +1,5 @@
 module InyxCatalogueRails
   class Engine < ::Rails::Engine
-    config.autoload_paths << File.expand_path("../lib/some/path", __FILE__)
     isolate_namespace InyxCatalogueRails
   end
 end
